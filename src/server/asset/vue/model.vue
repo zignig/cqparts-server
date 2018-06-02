0a0a315169b83cbc05ec3bd32d7097046c1417ab
Simon Kirkby <obeygiantrobot@gmail.com>
2018-06-02 05:35:56 +0000

    working multiload

--- src/server/asset/vue/model.vue
+++ src/server/asset/vue/model.vue
@@ -2,20 +2,24 @@
     <div class="pure-menu">
     <ul class="pure-menu-list">
         <li class="pure-menu-item" v-bind:model="modelList" v-for="obj in modelList">
-            <a class="pure-menu-link" v-on:click="load">{{ obj }}</a>
+            <a class="pure-menu-link" v-on:click="load(obj)">{{ obj }}</a>
         </li>
     </ul>
     </div>
 </template>
 
 <script>
-export default { 
+export default {
     props: {
-        modelList: [], 
+        modelList: {
+            type: Array,
+        },
     },
     methods:{
         load: function(event){
-            console.log(this);
+            clear();
+            load(event);
+            //console.log(event);
         },
         add: function(item){
             this.models.push(item);

b97def538441f9f62824fe19c56a0b3029971763
Simon Kirkby <obeygiantrobot@gmail.com>
2018-07-16 03:38:43 +0000

    pagination

--- src/server/asset/vue/vstrip.vue
+++ src/server/asset/vue/vstrip.vue
@@ -1,8 +1,8 @@
 <template>
-    <sui-segment v-show="modelList.length">
+    <sui-segment>
         <sui-input icon="search" placeholder="Search..."></sui-input>
         <sui-item-group divided >
-            <sui-item v-bind:model="modelList" :key="model.name" v-for="model in modelList">
+            <sui-item :key="model.name" v-for="model in paginatedData">
                 <sui-button compact basic v-on:click="load(model)">
                     <sui-item-image wrapped v-on:click="load(model)" size="tiny" v-show="model.img" :src="model.img">
                     </sui-item-image>
@@ -22,22 +22,38 @@
                 </sui-item-content>
             </sui-item>
         </sui-item-group>
-        <sui-button-group>
-            <sui-button>Prev</sui-button>
-            <sui-button>Next</sui-button>
+        <sui-button-group attached="bottom">
+            <sui-button @click="prevPage" :disabled="pageNumber === 0" icon="caret left" label-position="left">Prev</sui-button>
+            <sui-button @click="nextPage" :disabled="pageNumber >= pageCount -1" icon="caret right" label-position="right" floated="right">Next</sui-button>
         </sui-button-group>
     </sui-segment>
 </template>
 
 <script>
 export default {
+    data(){
+        return {
+            pageNumber: 0
+        }
+    },
     props: {
         current: "",
         modelList: {
             type: Array,
         },
+        size:{
+            type: Number,
+            required: false,
+            default: 6,
+        }
     },
     methods:{
+        nextPage: function(){
+            this.pageNumber++;
+        },
+        prevPage: function(){
+            this.pageNumber--;
+        },
         isActive: function(obj){
             if (this.current == obj.name){
                 return "green"
@@ -56,7 +72,19 @@ export default {
             this.modelList.splice(this.modelList.indexOf(index),1);
             EventBus.$emit('delete item',index);
         }
-    }
+    },
+    computed:{
+        pageCount(){
+            let l = this.modelList.length,
+                s = this.size;
+            return Math.floor(l/s);
+        },
+        paginatedData(){
+            const start = this.pageNumber * this.size,
+                end = start + this.size;
+            return this.modelList
+                .slice(start, end);
+        }}
 };
 
 </script>
